89eb0162328731b80634871e1fa014375966bc93
Eric Shen <eric.shen2000@gmail.com>
2017-01-05 15:51:14 -0500

    Add files via upload

---
+++ Path.java
@@ -0,0 +1,12 @@
+/**
+ * Created by erics on 1/5/2017.
+ */
+public class Path {
+
+
+    /**
+     * Basically, we first create the whole map, and then give this class a start and end node.
+     * The class will run a pathing algorithm, which generates an initial path.
+     */
+
+}
