3832b8aefc06e5f0acc1fae82ce30e0e010a4a0b
Eric Shen <eric.shen2000@gmail.com>
2017-01-05 17:24:55 -0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,15 @@
-import java.util.Map;
-
 public class Person {
 	public Path path;
 	public double distance; //distance along the path
 	public double speed; //amount to change location by each run unit
 
+	Map map = new Map();
+
 	public Person(Node start, Node end, double speed){
 
-
+		path = map.genPath(start, end);
+		distance = map.getDistance(path);
+		this.speed = speed;
 
 	}
 }
