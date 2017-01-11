de091c3691280a0a7b8b10ce07d7c23bb85e219c
Emma Jin <emmaljin@yahoo.com>
2017-01-10 21:41:46 -0500

    Changed it to be a bit less hard-code-y. The hallways are now generated using a method, but intersections are still hard coded in

--- src/Map.java
+++ src/Map.java
@@ -26,35 +26,61 @@ public class Map {
     }
 
     public void genRooms(){
+    	int x0; int y0; int x1; int y1; int interval; 
+    	int start; int end;
+    	
+    	int edge_cap = 5; //intersection edges
+    	int edge_pop = 5; 
+    	
+    	x0 = 235; y0 = 220; //Start of hallway
+    	
+    	//Top vertical hallway
+    	x1 = 235; y1 = 240; start = 0; interval = 10;
+    	end = ((y1 - y0)/interval) + start;
+    	
+       	intersections[0] = new Intersection(1, 2, 0, x1, y1);
+        genHall(start, y0, y1, x1, interval, 1);
+    	edges[2] = new Hallway(edge_cap, edge_pop, 0, rooms[end], intersections[0], intersections[0], rooms[end], x1, y1, 1);
+    	x0 = x1; y0 = y1;
+
+    	
+    	//Top horizontal hallway 1
+    	x1 = 305; y1 = 240; start = 3; interval = 15;
+    	end = ((x1 - x0)/interval) + start;
+    	
+    	intersections[1] = new Intersection(1, 2, 0, x1, y1);
+    	genHall(0, x0, x1, y1, interval, 0);
+    	edges[3] = new Hallway(edge_cap, edge_pop, 0, intersections[0], rooms[start], rooms[start], intersections[0], x0, y0, 1);
+    	intersections[0].routes[0] = edges[2];
+    	intersections[0].routes[1] = edges[3];
+    	edges[8] = new Hallway(edge_cap, edge_pop, 0, rooms[end], intersections[1], intersections[1], rooms[end], x1, y1, 1);
+    	x0 = x1; y0 = y1;
+    }
+    
+    public void genHall(int start, int aStart, int aEnd, int b, int interval, int vertical) {
+    	int end = (int)((aEnd-aStart)/interval)+1;
+    	
     	int room_size = 30;
     	int init_pop = 0;
     	int room_flow = 2;
     	
     	int edge_cap = 5;
     	int edge_pop = 5;
+    	int edge_init = 0;
     	
-    	//Top vertical hallway
-    	rooms[0] = new Room(room_size, room_flow, init_pop, 235, 220);
-    	rooms[1] = new Room(room_size, room_flow, init_pop, 235, 230);
-    	rooms[2] = new Room(room_size, room_flow, init_pop, 235, 240);
-    	edges[0] = new Hallway(edge_cap, edge_pop, 0, rooms[0], rooms[1], rooms[1], rooms[0], 235, 220, 1);
-    	edges[1] = new Hallway(edge_cap, edge_pop, 0, rooms[1], rooms[2], rooms[2], rooms[1], 235, 230, 1);
-    	
-
-    	intersections[0] = new Intersection(1, 2, 0, 235, 240);
-    	edges[2] = new Hallway(edge_cap, edge_pop, 0, rooms[2], intersections[0], intersections[0], rooms[2], 235, 240, 1);
-    	
-    	//Top horizontal hallway 1
-    	rooms[3] = new Room(room_size, room_flow, init_pop, 245, 240);
-    	rooms[4] = new Room(room_size, room_flow, init_pop, 260, 240);
-    	rooms[5] = new Room(room_size, room_flow, init_pop, 275, 240);
-    	rooms[6] = new Room(room_size, room_flow, init_pop, 290, 240);
-    	edges[3] = new Hallway(edge_cap, edge_pop, 0, intersections[0], rooms[3], rooms[3], intersections[0], 245, 240, 0);
-    	edges[4] = new Hallway(edge_cap, edge_pop, 0, rooms[3], rooms[4], rooms[4], rooms[3], 260, 240, 0);
-    	edges[5] = new Hallway(edge_cap, edge_pop, 0, rooms[4], rooms[5], rooms[5], rooms[4], 275, 240, 0);
-    	edges[6] = new Hallway(edge_cap, edge_pop, 0, rooms[5], rooms[6], rooms[6], rooms[5], 290, 240, 0);
-    	intersections[0].routes[0] = edges[2];
-    	intersections[0].routes[1] = edges[3];
+    	if (vertical == 0) {
+    		rooms[start] = new Room(room_size, room_flow, init_pop, aStart, b);
+    		for(int i = start+1; i < end; i++) {
+    			rooms[i] = new Room(room_size, room_flow, init_pop, aStart+i*interval, b);
+    			edges[i-1] = new Hallway(edge_cap, edge_pop, edge_init, rooms[i-1], rooms[i], rooms[i], rooms[i-1], aStart+i*interval, b, vertical);
+    		}
+    	}else{
+       		rooms[start] = new Room(room_size, room_flow, init_pop, b, aStart);
+    		for(int i = start+1; i < end; i++) {
+    			rooms[i] = new Room(room_size, room_flow, init_pop, b, aStart+i*interval);
+    			edges[i-1] = new Hallway(edge_cap, edge_pop, edge_init, rooms[i-1], rooms[i], rooms[i], rooms[i-1], b, aStart+i*interval, vertical);
+    		}
+    	}
     }
 
 }
