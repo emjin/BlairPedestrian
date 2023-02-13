a6f3eab24a89a4a8274c3be85c4ff81d1369ae4f
Emma Jin <emjin@users.noreply.github.com>
2023-02-13 14:07:07 -0800

    Delete Testing_three.java

--- Testing_three.java
+++
@@ -1,9 +0,0 @@
-class Testing {
-    void test(@RequestParam String value, HttpServletResponse response) {
-        System.out.println("Why even print things?");
-
-        Cookie cookie = new Cookie("cookie", value);
-        cookie.setSecure(false);
-        cookie.setHttpOnly(false);
-    }
-}
